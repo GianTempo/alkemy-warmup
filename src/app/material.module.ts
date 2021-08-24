d873173c2da0e3147054b389aa010487204783c2
GianTempo <giantemporetti@gmail.com>
2021-08-24 09:17:30 -0300

    Created Material module to host all material imports

---
+++ src/app/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+/* import { MaterialComponent } from ''; */
+
+@NgModule({
+    imports: [
+        CommonModule
+    ],
+    exports: [],
+    declarations: [],
+    providers: [],
+})
+export class MaterialModule { }
